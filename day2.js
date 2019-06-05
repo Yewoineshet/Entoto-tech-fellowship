1497619f8db5a026f367ddfffa72a6b120a2716e
Brad <yowielijah@gmail.com>
2019-06-05 10:21:52 +0200

    the coepen code

---
+++ day2.js
@@ -0,0 +1,20 @@
+var weatherTemprature = 37
+console.log(weatherTemprature)
+if(weatherTemprature <10){
+  console.log("it is cold outside")
+} else if(weatherTemprature >20)
+  console.log("it is warm outside")
+} else(weatherTemperature>=10 &&weatherTemprature<=20){
+  console.log("it is nice weather")
+} 
+function weatherTemperature(temprature){
+  console.log(weatherTemprature)
+if(temprature <10){
+  console.log(weatherTemperature("cold"))
+} else if(temprature >20)
+  console.log(weatherTemprature("warm"))
+} else(temperature>=10 &&weatherTemprature<=20){
+  console.log(weatherTemperature("nice"))
+} 
+
+}
